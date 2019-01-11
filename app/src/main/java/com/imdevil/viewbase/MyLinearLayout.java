86829580d064e75a15652eb2b6786765a2591883
mao.kun1 <516959708@qq.com>
2019-01-11 17:36:18 +0800

    滑动冲突

---
+++ app/src/main/java/com/imdevil/viewbase/MyLinearLayout.java
@@ -0,0 +1,63 @@
+package com.imdevil.viewbase;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+public class MyLinearLayout extends LinearLayout {
+    public MyLinearLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyLinearLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyLinearLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyLinearLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("MyFrameLayout","ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("MyFrameLayout","ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("MyFrameLayout","ACTION_UP");
+                break;
+            default:
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("MyFrameLayout","ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("MyFrameLayout","ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("MyFrameLayout","ACTION_UP");
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
